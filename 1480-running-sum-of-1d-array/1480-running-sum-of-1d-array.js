c1015707a1353cb5757da3f55fb1d05edd8cf03a
Mariah Morales <72050998+mariah622@users.noreply.github.com>
2022-05-11 13:31:23 -0400

    Time: 103 ms (20.39%), Space: 42.4 MB (54.25%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -4,11 +4,22 @@
  */
 var runningSum = function(nums) {
     let array = []
+//     make an empty array for our running sum
+    
     sum = 0
+//     sum starts at 0
+    
     for(let i = 0; i < nums.length; i++){
+//         iterate through every number in our nums array
+        
         sum = sum + nums[i]
+//         sum is equal to the new sum plus the next number in our nums array
+        
         array.push(sum);
+//         push our new sum into our new array
     }
+    
     return array
+//     We return our array
     
 };
