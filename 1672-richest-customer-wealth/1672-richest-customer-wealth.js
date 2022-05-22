d4bd7b106d74f06fe434b433fd7deb4b606ce1ab
Mariah Morales <72050998+mariah622@users.noreply.github.com>
2022-05-22 13:30:58 -0400

    Time: 82 ms (43.74%), Space: 42.2 MB (54.39%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let maxWealth = 0
+    for(let i = 0; i < accounts.length; i++){
+        let sum = 0;
+        for(let j = 0; j < accounts[i].length; j++){
+            sum = sum += accounts[i][j];
+        }
+        
+        if (sum > maxWealth){
+            maxWealth = sum
+        }
+    }
+    return maxWealth
+    
+    
+};
