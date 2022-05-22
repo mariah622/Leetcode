823eccaffdc13fe2120ba983c35a32cb586c00c8
Mariah Morales <72050998+mariah622@users.noreply.github.com>
2022-05-22 14:00:37 -0400

    Time: 85 ms (62.45%), Space: 44.2 MB (65.97%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let array = [];
+    
+    for(let i= 0; i < n; i++){
+        array.push(nums[i]);
+        array.push(nums[i+n]);   
+        
+    }
+    return array
+    
+};
